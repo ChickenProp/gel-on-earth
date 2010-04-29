c4a17a602403e776b83f58f1fd60a3098172a0a1
Philip Hazelden <philip.hazelden@gmail.com>
2010-04-29 16:38:13 +0100

    Close window.

--- src/includes.h
+++ src/includes.h
@@ -3,6 +3,7 @@
 
 #include <SFML/System.hpp>
 #include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
 
 #include <iostream>
 
--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,19 @@
 
 int main()
 {
-	sf::Window window(sf::VideoMode(640, 480, 32), "Gel on Earth");
-	window.Display();
+	sf::RenderWindow window(sf::VideoMode(640, 480, 32), "Gel on Earth");
+	sf::Clock clock;
+       
+	while (window.IsOpened()) {
+		sf::Event ev;
+		while (window.GetEvent(ev)) {
+			if (ev.Type == sf::Event::Closed)
+				window.Close();
+		}
 
-	sf::Clock Clock;
-	while (Clock.GetElapsedTime() < 5.f) {
-		std::cout << Clock.GetElapsedTime() << std::endl;
+		window.Clear(sf::Color(255,255,255));
+		window.Display();
+		std::cout << clock.GetElapsedTime() << std::endl;
 		sf::Sleep(0.5f);
 	}
 
