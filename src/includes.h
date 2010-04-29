617a90154b9f8eb1035e23e65460135f97e92576
Philip Hazelden <philip.hazelden@gmail.com>
2010-04-29 10:43:26 +0100

    Display a window.

---
+++ src/includes.h
@@ -0,0 +1,9 @@
+#ifndef _INCLUDES_H
+#define _INCLUDES_H
+
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
+
+#include <iostream>
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,15 @@
-#include <SFML/System.hpp>
-#include <iostream>
+#include "includes.h"
 
 int main()
 {
-    sf::Clock Clock;
-    while (Clock.GetElapsedTime() < 5.f)
-    {
-        std::cout << Clock.GetElapsedTime() << std::endl;
-        sf::Sleep(0.5f);
-    }
+	sf::Window window(sf::VideoMode(640, 480, 32), "Gel on Earth");
+	window.Display();
 
-    return 0;
+	sf::Clock Clock;
+	while (Clock.GetElapsedTime() < 5.f) {
+		std::cout << Clock.GetElapsedTime() << std::endl;
+		sf::Sleep(0.5f);
+	}
+
+	return 0;
 }
