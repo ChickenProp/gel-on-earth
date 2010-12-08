fc93d57e3c8fe3c237f84f06066d0ac8e1586e3c
Philip Hazelden <philip.hazelden@gmail.com>
2010-12-08 15:49:19 +0000

    Use Vertexes for debug drawing.

--- src/debug-draw.cpp
+++ src/debug-draw.cpp
@@ -1,14 +1,15 @@
 #include "debug-draw.h"
+#include "vertex.h"
 
 void DebugDraw::drawLine(const btVector3 &from, const btVector3 &to,
                          const btVector3 &color)
 {
-	glColor3f(color.x(), color.y(), color.z());
+	Vertex line[] = {
+		Vertex(from, color.x(), color.y(), color.z()),
+		Vertex(to, color.x(), color.y(), color.z())
+	};
 
-	glBegin(GL_LINES);
-	glVertex3f(from.x(), from.z(), from.y());
-	glVertex3f(to.x(), to.z(), to.y());
-	glEnd();
+	line->draw(GL_LINES, 2);
 }
 
 void DebugDraw::setDebugMode(int m) {
@@ -29,4 +30,3 @@ void DebugDraw::reportErrorWarning(const char*) {
 void DebugDraw::draw3dText(const btVector3&, const char*) {
 	printf("draw3dText()");
 }
-
